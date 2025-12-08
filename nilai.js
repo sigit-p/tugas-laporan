8db62e578761b5745d487b54fd2c59db7911a953
sigit_p <35914314+sigit-p@users.noreply.github.com>
2025-12-08 21:43:50 +0700

    Update nilai.js

--- nilai.js
+++ nilai.js
@@ -3,7 +3,7 @@
 // ==============================================================================
 
 // PASTI GANTI URL INI!
-const API_URL = 'https://script.google.com/macros/s/AKfycbwswDuj1YQHP4C6fXfdEa1G1rqW6hvbx6ZCnnfsRJsHC1fb5byCpHtMmU0vIZBgoYqaPg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwswDuj1YQHP4C6fXfdEa1G1rqW6hvbx6ZCnnfsRJsHC1fb5byCpHtMmU0vIZBgoYqaPg/dev';
 
 // Daftar nama job (harus sama persis dengan header di Sheets, E sampai K)
 const jobNames = [
